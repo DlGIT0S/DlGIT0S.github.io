24993ea421e18f242276c660f37b0d314d690ea3
DlGIT0S <digit0srblx@gmail.com>
2024-05-14 22:35:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('discordButton').addEventListener('click', function() {
+    window.location.href = 'https://discord.com/invite/your-discord-link';
+});
+
+document.getElementById('scriptsButton').addEventListener('click', function() {
+    alert('Scripts functionality will be added soon!');
+});
