29a1d9757e7190d95f12cc0cd1aa927a987f6b39
DlGIT0S <digit0srblx@gmail.com>
2024-05-18 15:22:58 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -9,14 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // Open the YouTube page in a new tab
         window.open('https://youtube.com/@dlgit0s?si=Scvzc9bf9T_B3Ra-', '_blank');
 
-        // Check if the user subscribed (this is a simulation)
-        var isSubscribed = confirm('Did you subscribe to our YouTube channel?');
-        if (isSubscribed) {
-            // Unlock the key system
-            alert('Thank you for subscribing! The key system has been unlocked.');
-        } else {
-            // Notify the user that they need to subscribe to unlock the key system
-            alert('Please subscribe to our YouTube channel to unlock the key system.');
-        }
+        // Unlock the key system
+        alert('Thank you for subscribing! The key system has been unlocked.');
     });
 });
